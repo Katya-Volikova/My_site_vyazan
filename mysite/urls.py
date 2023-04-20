b4b6a5086451491e250e7e8f5b571011e634d0ed
HONOR <volikovae9@gmail.com>
2023-04-20 21:09:43 +0300

    Commit 20.04 last

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,17 +5,10 @@ from pools.controllers import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index.html/', index.index),
-    path('index.html/about_us.html/', index.about_us),
-    path('index.html/log_in.html', index.log_in),
-    path('index.html/sign_in.html', index.sign_in),
-    path('index.html/forget.html', index.forget),
-    path('index.html/basket.html', index.basket),
-#    path('/catalog/', index.catalog),
-
-
-
-    # path('lol.html/about2.html', index.about),
-    # path('lol.html/index2.html', index.index),
-    # path('lol.html/care.html', index.care),
+    path('about_us.html/', index.about_us),
+    path('log_in.html', index.log_in),
+    path('sign_in.html', index.sign_in),
+    path('forget.html', index.forget),
+    path('basket.html', index.basket),
 
 ]
--- pools/static/pools/js_files/basket.js
+++ pools/static/pools/js_files/basket.js
@@ -1,27 +1,36 @@
 let cart = {
     'blue_switer': 0,
+    'green_hat': 0,
+    'white_switer': 0,
 };
 
-document.onclick = event =>{
+let basket_icon = document.getElementById("one_pict1");
+basket_icon.onclick = event =>{
     console.log(event.target.classList);
-    if(event.target.classList.contains('plas_basket')){
-        plasFunction(event.target.dataset);
-    }
-    if(event.target.classList.contains('menos_basket')){
-        menosFunction(event.target.dataset);
-    }
-    if(event.target.classList.contains('cancel')){
-        canselFunction(event.target.dataset);
-    }
+//    if(event.target.classList.contains('plas_basket')){
+//        plasFunction(event.target.dataset);
+//    }
+//    if(event.target.classList.contains('menos_basket')){
+//        menosFunction(event.target.dataset);
+//    }
+//    if(event.target.classList.contains('cancel')){
+//        canselFunction(event.target.dataset);
+//    }
+//   if(event.target.classList.contains('card-img_buy')){
+//       event.stopPropagation();
+    cart['blue_switer']++;
+    console.log(cart);
+      /* plasFunction(event.target.dataset);*/
+// }
 }
 
 const plasFunction = id => {
-console.log(id);
-    cart[id.id]++;
-    renderCart();
-}
+    cart[1]++;
+    console.log(cart[1]);
+    console.log(cart);
+ }
 
-const menosFunction = id => {
+/*const menosFunction = id => {
     if(cart[id.id]-1==0){
         deleteFunction(id);
         return true;
@@ -42,9 +51,10 @@ const canselFunction = id => {
     const cartItem = document.querySelector(".construct__wrapper-text_basket");
     cartItem.classList.add('non_visible');
     renderCart();
-}
+}*/
+/*
 const renderCart = () => {
     const count = document.querySelector('#count')
     count.innerHTML = cart.blue_switer;
     console.log(cart);
-}
+}*/
