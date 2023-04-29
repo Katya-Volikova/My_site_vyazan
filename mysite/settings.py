e45425ee32eb30eb8b23af805673811ba013ca8d
HONOR <volikovae9@gmail.com>
2023-04-29 14:30:16 +0300

    Commit 29.04 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,19 +78,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
-#'default': {
-#    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#    'NAME': 'django',
-#    'USER': 'postgres',
-#    'PASSWORD': '1234',
-#    'HOST': '127.0.0.1',
-#    'PORT': '5432',
-#}
 
 
 # Password validation
---
+++ pools/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Feedback(models.Model):
+    email = models.CharField(max_length=255)
+    text = models.TextField()
