fe802a47fb4049917d9637d4e47fabd79a80a516
HONOR <volikovae9@gmail.com>
2023-04-29 16:39:41 +0300

    Commit 29.04 3

--- pools/models.py
+++ pools/models.py
@@ -1,6 +1,7 @@
-from django.db import models
+from django.db import migrations, models
 
 
 class Feedback(models.Model):
     email = models.CharField(max_length=255)
     text = models.TextField()
+
