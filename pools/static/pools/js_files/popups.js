b3a242a2ab43a1b492e5f7b4ca3c9f08052f673a
HONOR <volikovae9@gmail.com>
2023-05-13 17:21:46 +0300

    Commit 13.05 1 корзина

--- pools/static/pools/js_files/popups.js
+++ pools/static/pools/js_files/popups.js
@@ -1,4 +1,7 @@
 
+
+
+
 let flag = 0;
 function openForm() {
   if(flag == 0){
@@ -90,6 +93,17 @@ function openForm() {
         if(flag == 0) document.getElementById("popupFormMain21").style.display = "block";
         flag = 1;
       }
+      function openPopup22() {
+        if(flag == 0) document.getElementById("popupFormMain22").style.display = "block";
+        flag = 1;
+      }
+      function openPopupCart() {
+        if(flag == 0) document.getElementById("popupCart").style.display = "block";
+        flag = 1;
+        showCart();
+}
+
+
       function closePopup() {
         document.getElementById("popupFormMain").style.display = "none";
         document.getElementById("popupFormMain2").style.display = "none";
@@ -112,111 +126,11 @@ function openForm() {
         document.getElementById("popupFormMain19").style.display = "none";
         document.getElementById("popupFormMain20").style.display = "none";
         document.getElementById("popupFormMain21").style.display = "none";
+        document.getElementById("popupFormMain22").style.display = "none";
+        document.getElementById("popupCart").style.display = "none";
         document.getElementById("popupForm").style.display = "none";
         flag = 0;
       }
-
-
-// JavaScript код для добавления товара в корзину
-
-/*
-const cart = [];
-
-function addToCart(product, price, image) {
-  let itemIndex = -1;
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].product === product) {
-      itemIndex = i;
-      break;
-    }
-  }
-
-  if (itemIndex === -1) {
-    const item = { product, price, count: 1, image };
-    cart.push(item);
-  } else {
-    cart[itemIndex].count++;
-  }
-
-  console.log(Товар ${product} добавлен в корзину);
-
-}
-
-function showCart() {
-  const cartElement = document.getElementById('cart');
-  let cartHTML = '';
-
-  if (cart.length === 0) {
-    cartHTML = '<p>Корзина пуста</p>';
-  } else {
-    cartHTML = '<ul>';
-    for (let item of cart) {
-      cartHTML += <li><img src="${item.image}" width="50"> ${item.product} - ${item.price} руб. x ${item.count}</li>;
-      total += item.price * item.count
-    }
-    cartHTML += '</ul>';
-  }
-
-  cartElement.innerHTML = cartHTML;
-}
-
-showCart(); // вызываем функцию для отображения корзины при загрузке страницы (модальное окно)
-
-const cartIcon = document.getElementById('cart');
-const cartModal = document.getElementById('cartModal');
-const cartItems = document.getElementById('cartItems');
-const closeButton = document.querySelector('#cartModal .close');
-
-cartIcon.addEventListener('click', () => {
-  cartModal.style.display = 'block';
-  showCartItems();
-});
-
-// закрываем по нажатию на фон
-window.onclick = function(event) {
-  if (event.target == cartModal) {
-    cartModal.style.display = 'none';
-  }
-}
-
-
-function showCartItems() {
-  let cartHTML = '';
-
-  if (cart.length === 0) {
-    cartHTML = '<p>Корзина пуста</p>';
-  } else {
-    cartHTML = '<ul>';
-    for (let item of cart) {
-
-      cartHTML += <li><img src="${item.image}" width="50"> ${item.product} - ${item.price} руб. x ${item.count}</li>;
-
-    }
-    cartHTML += '</ul>';
-  }
-
-  cartItems.innerHTML = cartHTML;
-}
-
-
-closeButton.addEventListener('click', () => {
-  cartModal.style.display = 'none';
-});
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ pools/static/pools/js_files/shop_cart.js
@@ -0,0 +1,139 @@
+const cart = [];
+let itemCount = 0;
+let AllPrice = 0;
+let flag_null=0;
+
+alert("trash")
+function addToCart(product, price, image) {
+    alert("lol");
+    let index = -1;
+
+    //присваиваем индекс для товара
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].product === product) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index === -1) {
+        const item = { product, price, count: 1, image };
+        cart.push(item);
+        console.log(item);
+    } else {
+        cart[index].count++;
+    }
+
+}
+
+function showCart() {
+    const itemsContainer = document.getElementById('cartItems');
+    let cartHTML = "";
+
+    if ((cart.length === 0) && ( flag_null !== 1)) {
+        cartHTML = "<p id='empty'>Корзина пуста</p>";
+        flag_null=1;
+    } else if (cart.length > 0){
+        if(flag_null===1){
+            document.getElementById('empty').remove();
+            flag_null=1;
+        }
+
+        cartHTML = `<ul>`;
+        for (let i=0;i <cart.length;i++) {
+            cartHTML += `<li>${cart[i].product} - ${cart[i].price}руб - ${cart[i].count}шт</li>`
+/*
+            cartHTML += '<img src=${cart.item.image} width="50"/>';
+             cartHTML += '</li>';*/
+            /*AllPrice += cart.item.price * cart.item.count;*/
+        }
+        cartHTML += `</ul>`;
+    }
+
+    itemsContainer.innerHTML += cartHTML;
+}
+
+showCart();
+
+// JavaScript код для добавления товара в корзину
+/*const cart = [];
+
+function addToCart(product, price, image) {
+  let itemIndex = -1;
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].product === product) {
+      itemIndex = i;
+      break;
+    }
+  }
+
+  if (itemIndex === -1) {
+    const item = { product, price, count: 1, image };
+    cart.push(item);
+  } else {
+    cart[itemIndex].count++;
+  }
+
+  console.log("Товар ${product} добавлен в корзину");
+
+}
+
+function showCart() {
+  const cartElement = document.getElementById('cart');
+  let cartHTML = '';
+
+  if (cart.length === 0) {
+    cartHTML = '<p>Корзина пуста</p>';
+  } else {
+    cartHTML = '<ul>';
+    for (let item of cart) {
+      cartHTML += <li><img src="${item.image}" width="50"> ${item.product} - ${item.price} руб. x ${item.count}</li>;
+   total += item.price * item.count;
+    cartHTML += </ul>;
+  }
+
+  cartElement.innerHTML = cartHTML;
+}
+
+showCart(); // вызываем функцию для отображения корзины при загрузке страницы (модальное окно)
+
+const cartIcon = document.getElementById('cart');
+const cartModal = document.getElementById('cartModal');
+const cartItems = document.getElementById('cartItems');
+const closeButton = document.querySelector('#cartModal .close');
+
+cartIcon.addEventListener('click', () => {
+  cartModal.style.display = 'block';
+  showCartItems();
+});
+
+// закрываем по нажатию на фон
+window.onclick = function(event) {
+  if (event.target == cartModal) {
+    cartModal.style.display = 'none';
+  }
+}
+
+
+function showCartItems() {
+  let cartHTML = '';
+
+  if (cart.length === 0) {
+    cartHTML = '<p>Корзина пуста</p>';
+  } else {
+    cartHTML = '<ul>';
+    for (let item of cart) {
+
+      cartHTML += <li><img src="${item.image}" width="50"> ${item.product} - ${item.price} руб. x ${item.count}</li>;
+
+    }
+    cartHTML+=</ul>;
+  }
+
+  cartItems.innerHTML = cartHTML;
+}
+
+
+closeButton.addEventListener('click', () => {
+  cartModal.style.display = 'none';
+});*/
